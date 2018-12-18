63d4b82b90506fde2364bd38a23206906cda12b7
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2018-12-18 15:36:41 +0000

    A little bit of code cleanup and commenting

--- uk.ac.kcl.inf.mazegame.basicExample/src/uk/ac/kcl/inf/mazegame/Main.java
+++ uk.ac.kcl.inf.mazegame.basicExample/src/uk/ac/kcl/inf/mazegame/Main.java
@@ -13,15 +13,16 @@ public class Main {
 		MazeGame mg = new MazeGame();
 		Room startRoom = new Room("You're in a dark and dank room.");
 		Room secondRoom = new Room("You're in a brightly lit room.");
+		// Note how all room walls need to be initialised (yes, I could have done this in the constructor, I know)
 		for (Direction d: Direction.values()) {
 			startRoom.setSide(d, new Wall());
-		}
-		for (Direction d: Direction.values()) {
 			secondRoom.setSide(d, new Wall());
 		}
 		DoorWall dw = new DoorWall(startRoom, secondRoom);
+		// By default doors are closed -- I haven't put in anything at the moment to allow players to open doors, so need to make sure they're open
 		dw.open();
 		startRoom.setSide(Direction.NORTH, dw);
+		// Note the logical error in choice of direction here...
 		secondRoom.setSide(Direction.NORTH, dw);
 		mg.addRoom(startRoom);
 		mg.addRoom(secondRoom);
